916d78823bbce4c8e452f32447044d909f2c674b
ricobsj <ricobsj@localhost>
2020-03-01 18:51:02 +0100

    Second Repo

--- WebContent/inc/menu.jsp
+++ WebContent/inc/menu.jsp
@@ -2,11 +2,11 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <div id="menu">
 	<p>
-		<a href="<c:url value="/WEB-INF/jsp/creationClient"/>">Créer un
+		<a href="<c:url value="/WEB-INF/jsp/creerClient"/>">Créer un
 			nouveau client</a>
 	</p>
 	<p>
-		<a href="<c:url value="/WEB-INF/jsp/creationCommande"/>">Créer une
+		<a href="<c:url value="/WEB-INF/jsp/creerCommande"/>">Créer une
 			nouvelle commande</a>
 	</p>
 </div>
--- src/com/sdzee/pro/servlets/CreationCommande.java
+++ src/com/sdzee/pro/servlets/CreationCommande.java
@@ -3,6 +3,7 @@ package com.sdzee.pro.servlets;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.sdzee.pro.beans.Commande;
 import com.sdzee.pro.forms.CreationCommandeForm;
 
+@WebServlet( "/creationCommande" )
 public class CreationCommande extends HttpServlet {
 
     private static final long  serialVersionUID = 1L;
