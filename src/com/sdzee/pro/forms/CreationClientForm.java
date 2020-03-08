419e997b5e76c919598ce2531b7234213dfe0d4a
ricobsj <ricobsj@localhost>
2020-03-08 23:48:51 +0100

    First Repo

--- src/com/sdzee/pro/forms/CreationClientForm.java
+++ src/com/sdzee/pro/forms/CreationClientForm.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import com.sdzee.pro.beans.Client;
 
 public final class CreationClientForm {
+
     private static final String CHAMP_NOM       = "nomClient";
     private static final String CHAMP_PRENOM    = "prenomClient";
     private static final String CHAMP_ADRESSE   = "adresseClient";
@@ -26,6 +27,7 @@ public final class CreationClientForm {
     }
 
     public Client creerClient( HttpServletRequest request ) {
+
         String nom = getValeurChamp( request, CHAMP_NOM );
         String prenom = getValeurChamp( request, CHAMP_PRENOM );
         String adresse = getValeurChamp( request, CHAMP_ADRESSE );
@@ -39,6 +41,7 @@ public final class CreationClientForm {
         } catch ( Exception e ) {
             setErreur( CHAMP_NOM, e.getMessage() );
         }
+
         client.setNom( nom );
 
         try {
@@ -46,6 +49,7 @@ public final class CreationClientForm {
         } catch ( Exception e ) {
             setErreur( CHAMP_PRENOM, e.getMessage() );
         }
+
         client.setPrenom( prenom );
 
         try {
@@ -53,6 +57,7 @@ public final class CreationClientForm {
         } catch ( Exception e ) {
             setErreur( CHAMP_ADRESSE, e.getMessage() );
         }
+
         client.setAdresse( adresse );
 
         try {
@@ -60,6 +65,7 @@ public final class CreationClientForm {
         } catch ( Exception e ) {
             setErreur( CHAMP_TELEPHONE, e.getMessage() );
         }
+
         client.setTelephone( telephone );
 
         try {
@@ -67,72 +73,71 @@ public final class CreationClientForm {
         } catch ( Exception e ) {
             setErreur( CHAMP_EMAIL, e.getMessage() );
         }
+
         client.setEmail( email );
 
         if ( erreurs.isEmpty() ) {
-            resultat = "Succès de la création du client.";
+            resultat = "Succès de la création de client.";
         } else {
-            resultat = "Échec de la création du client.";
+            resultat = "Echec de la creation client.";
         }
 
         return client;
     }
 
     private void validationNom( String nom ) throws Exception {
+
         if ( nom != null ) {
             if ( nom.length() < 2 ) {
                 throw new Exception( "Le nom d'utilisateur doit contenir au moins 2 caractères." );
+            } else {
+                throw new Exception( "Merci d'entrer un nom d'utilisateur." );
             }
-        } else {
-            throw new Exception( "Merci d'entrer un nom d'utilisateur." );
         }
     }
 
     private void validationPrenom( String prenom ) throws Exception {
+
         if ( prenom != null && prenom.length() < 2 ) {
-            throw new Exception( "Le prénom d'utilisateur doit contenir au moins 2 caractères." );
+            throw new Exception( "Le prenom d'utilisateur doit contenir au moins 2 caractères." );
+        } else {
+            throw new Exception( "Merci d'entrer un prenom d'utilisateur." );
         }
     }
 
     private void validationAdresse( String adresse ) throws Exception {
-        if ( adresse != null ) {
-            if ( adresse.length() < 10 ) {
-                throw new Exception( "L'adresse de livraison doit contenir au moins 10 caractères." );
-            }
+
+        if ( adresse != null && adresse.length() < 10 ) {
+            throw new Exception( "L'adresse de livraison doit contenir au moins 10 caractères." );
         } else {
             throw new Exception( "Merci d'entrer une adresse de livraison." );
         }
     }
 
     private void validationTelephone( String telephone ) throws Exception {
+
         if ( telephone != null ) {
             if ( !telephone.matches( "^\\d+$" ) ) {
                 throw new Exception( "Le numéro de téléphone doit uniquement contenir des chiffres." );
             } else if ( telephone.length() < 4 ) {
                 throw new Exception( "Le numéro de téléphone doit contenir au moins 4 chiffres." );
+            } else {
+                throw new Exception( "Merci d'entrer un numéro de téléphone" );
             }
-        } else {
-            throw new Exception( "Merci d'entrer un numéro de téléphone." );
         }
     }
 
     private void validationEmail( String email ) throws Exception {
+
         if ( email != null && !email.matches( "([^.@]+)(\\.[^.@]+)*@([^.@]+\\.)+([^.@]+)" ) ) {
             throw new Exception( "Merci de saisir une adresse mail valide." );
         }
     }
 
-    /*
-     * Ajoute un message correspondant au champ spécifié à la map des erreurs.
-     */
     private void setErreur( String champ, String message ) {
         erreurs.put( champ, message );
     }
 
-    /*
-     * Méthode utilitaire qui retourne null si un champ est vide, et son contenu
-     * sinon.
-     */
     private static String getValeurChamp( HttpServletRequest request, String nomChamp ) {
         String valeur = request.getParameter( nomChamp );
         if ( valeur == null || valeur.trim().length() == 0 ) {
